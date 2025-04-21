e0412d17435cf5d5ef30172da1c7a4bf37b610a6
suvetha <113636722+suve0803@users.noreply.github.com>
2025-04-21 15:05:25 +0530

    Add files via upload

---
+++ day01.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=20 , b=50;
+    cout<<"Addition(a+b):"<<(a+b)<<endl;
+    cout<<"Subraction(a-b):"<<(a-b)<<endl;
+    cout<<"Multiplication(a*b):"<<(a*b)<<endl;
+    cout<<"division(a/b):"<<(a/b)<<endl;
+    return 0;
+}
